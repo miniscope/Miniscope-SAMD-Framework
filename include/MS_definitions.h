29874ea47a06182e8c737c9a9fad0a4598aa4493
t-sasatani <sasatani.dev@gmail.com>
2023-05-24 10:28:01 -0700

    added peripheral config

--- include/MS_definitions.h
+++ include/MS_definitions.h
@@ -14,10 +14,10 @@
 #include <hpl_pcc_config.h>
 #include <hpl_dma.h>
 
-// Firmware mode
-//#define V4WF_MODE
+// ------ FIRMWARE MODE ------------------------
+#define V4WF_MODE
 //#define WLMS_MODE
-#define DMA_TO_SPI_TESTMODE
+//#define DMA_TO_SPI_TESTMODE
 //#define DMA_TO_SPI_METRO_TESTMODE
 
 // Peripheral enable based on mode
@@ -34,7 +34,7 @@
 #define EWL_ENABLE
 #define PUSH_BUT_ENABLE
 #define STATUS_LED_ENABLE
-#define IR_RX_ENABLE
+#define IR_TRIGGER_ENABLE
 #endif
 
 #ifdef WLMS_MODE
@@ -46,7 +46,7 @@
 #define EWL_ENABLE
 #define PUSH_BUT_ENABLE
 #define STATUS_LED_ENABLE
-#define IR_RX_ENABLE
+#define IR_UART_ENABLE
 #endif
 
 #ifdef DMA_TO_SPI_TESTMODE
