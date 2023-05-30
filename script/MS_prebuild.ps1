5fb8a982eae66d7261ccd5a187078630ce9b96ac
t-sasatani <sasatani.dev@gmail.com>
2023-05-30 16:43:37 -0700

    nest shellscript in a function

--- script/MS_prebuild.ps1
+++ script/MS_prebuild.ps1
@@ -10,71 +10,72 @@ $submodulepath = Resolve-Path "..\..\MS_module\"
 
 # Array of excluded c files and header files
 $cfilepatharray = @("sd_mmc\sd_mmc.c", "hal\src\hal_mci_sync.c", "hpl\dmac\hpl_dmac.c", "hpl\sdhc\hpl_sdhc.c")
-$headerpatharray = @("sd_mmc\sd_mmc.h", "hal\include\hal_mci_sync.h", "hal\include\hpl_mci_sync.h")
+$headerpatharray = @("sd_mmc\sd_mmc.h", "hal\include\hal_mci_sync.h", "hal\include\hpl_mci_sync.h", "hal\include\hpl_dma.h")
 
-
-For ($i=0; $i -lt $cfilepatharray.Length; $i++) {
-    $originalfiledir = Split-Path -Path ($prjroot.Path + "\" + $cfilepatharray[$i]) #directory of original file
-    $originalfilename = Split-Path -Path ($prjroot.Path + "\" + $cfilepatharray[$i]) -Leaf #name of original file
-
-    # change original .c file to .ctmp file to avoid compile
-    if (Test-Path -Path ($originalfiledir + "\" + $originalfilename) -PathType Leaf) {
-        try {
-            #$filepath = Get-ChildItem ($originalfiledir + "\" + $originalfilename)
-
-            # if .ctmp file exists, delete first
-            if (Test-Path -Path ($originalfiledir + "\" + $originalfilename + "tmp") -PathType Leaf) {
-                Remove-Item ($originalfiledir + "\" + $originalfilename + "tmp")
-                Write-Host ($originalfiledir + "\" + $originalfilename + "tmp has been deleted.")
+function Install-Drivers {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [String[]]$DriverList
+    )
+    For ($i=0; $i -lt $DriverList.Length; $i++) {
+        $originalfiledir = Split-Path -Path ($prjroot.Path + "\" + $DriverList[$i]) #directory of original file
+        $originalfilename = Split-Path -Path ($prjroot.Path + "\" + $DriverList[$i]) -Leaf #name of original file
+    
+        # change original .c file to .ctmp file to avoid compile
+        if (Test-Path -Path ($originalfiledir + "\" + $originalfilename) -PathType Leaf) {
+            try {
+                #$filepath = Get-ChildItem ($originalfiledir + "\" + $originalfilename)
+    
+                # if .ctmp file exists, delete first
+                if (Test-Path -Path ($originalfiledir + "\" + $originalfilename + "tmp") -PathType Leaf) {
+                    Remove-Item ($originalfiledir + "\" + $originalfilename + "tmp")
+                    Write-Host ($originalfiledir + "\" + $originalfilename + "tmp has been deleted.")
+                }
+                # change .c file to .ctmp file
+                Rename-Item -Path ($originalfiledir + "\" + $originalfilename) -NewName ($originalfilename + "tmp")
+                Write-Host ($originalfilename + "tmp has been created.")
             }
-            # change .c file to .ctmp file
-            Rename-Item -Path ($originalfiledir + "\" + $originalfilename) -NewName ($originalfilename + "tmp")
-            Write-Host ($originalfilename + "tmp has been created.")
+            catch {
+                throw $_.Exception.Message
+            }
+        }
+        # copy custom file into original directory
+        try {
+            $customfile = Get-Childitem $submodulepath -Filter ($originalfilename + "src") -Recurse
+            New-Item -ItemType HardLink -Path ($originalfiledir + "\" + $originalfilename) -Value $customfile.FullName
+            Write-Host ($originalfiledir + "\" + $originalfilename + " linked to " + $customfile.Name)            
         }
         catch {
             throw $_.Exception.Message
         }
     }
-    # copy custom file into original directory
+}
+
+function Install-Main {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [System.Management.Automation.PathInfo]$FilePath,
+
+        [Parameter()]
+        [System.Management.Automation.PathInfo]$HardLinkPath
+    )
+    $CustomMain = Get-Childitem $FilePath -Filter ("main.csrc")
+    $DefaultMain = Get-Childitem $HardLinkPath -Filter ("main.c")
+    if (Test-Path -Path ($DefaultMain.FullName) -PathType Leaf) {
+        Remove-Item $DefaultMain.FullName
+        Write-Host ($DefaultMain.FullName + "\main.c has been deleted.")
+    }
     try {
-        $customfile = Get-Childitem $submodulepath -Filter ($originalfilename + "src") -Recurse
-        New-Item -ItemType HardLink -Path ($originalfiledir + "\" + $originalfilename) -Value $customfile.FullName
-        Write-Host ($originalfiledir + "\" + $originalfilename + " linked to " + $customfile.Name)            
+        New-Item -ItemType HardLink -Path $DefaultMain.FullName -Value $CustomMain.FullName
+        Write-Host ($CustomMain.FullName + " linked to " + $DefaultMain.FullName)                    
     }
     catch {
         throw $_.Exception.Message
     }
 }
 
-For ($i=0; $i -lt $headerpatharray.Length; $i++) {
-    $originalfiledir = Split-Path -Path ($prjroot.Path + "\" + $headerpatharray[$i]) #directory of original file
-    $originalfilename = Split-Path -Path ($prjroot.Path + "\" + $headerpatharray[$i]) -Leaf #name of original file
-
-    # change original .c file to .ctmp file to avoid compile
-    if (Test-Path -Path ($originalfiledir + "\" + $originalfilename) -PathType Leaf) {
-        try {
-            #$filepath = Get-ChildItem ($originalfiledir + "\" + $originalfilename)
-
-            # if .ctmp file exists, delete first
-            if (Test-Path -Path ($originalfiledir + "\" + $originalfilename + "tmp") -PathType Leaf) {
-                Remove-Item ($originalfiledir + "\" + $originalfilename + "tmp")
-                Write-Host ($originalfiledir + "\" + $originalfilename + "tmp has been deleted.")
-            }
-            # change .c file to .ctmp file
-            Rename-Item -Path ($originalfiledir + "\" + $originalfilename) -NewName ($originalfilename + "tmp")
-            Write-Host ($originalfilename + "tmp has been created.")
-        }
-        catch {
-            throw $_.Exception.Message
-        }
-    }
-    # copy custom file into original directory
-    try {
-        $customfile = Get-Childitem $submodulepath -Filter ($originalfilename + "src") -Recurse
-        New-Item -ItemType HardLink -Path ($originalfiledir + "\" + $originalfilename) -Value $customfile.FullName
-        Write-Host ($originalfiledir + "\" + $originalfilename + " linked to " + $customfile.Name)            
-    }
-    catch {
-        throw $_.Exception.Message
-    }
-}
+Install-Drivers -DriverList $cfilepatharray
+Install-Drivers -DriverList $headerpatharray
+Install-Main -FilePath $submodulepath -HardLinkPath $prjroot
