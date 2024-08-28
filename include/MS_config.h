7581309fdecd9c8a32f7f1f2692b6263ffb8ba9c
MarcelMB <marcelbrosch.mb@gmail.com>
2024-08-28 13:11:51 -0700

    added y-axis ROI shift (to existing x ROI shift)

--- include/MS_config.h
+++ include/MS_config.h
@@ -64,6 +64,7 @@ The modes should be a set of peripherals used in the specified mode.
 #define BUFFER_BLOCK_LENGTH		10 // can be edited by user to optimize speed
 #define NUM_BUFFERS				32 // Assumes 1024 KB program memory
 #define ROI_XSHIFT_PX			150 // x shift
+#define ROI_YSHIFT_PX			100 // y shift
 #define EXCITATION_LED_VALUE	1// Brightness value of excitation LED: from 0 to 100.
 #define DUMMY_WORD_LENGTH				10
 #define DUMMY_WORD				0xff00cccc
